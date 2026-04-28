23f5b8ed9e4786dbd317fc0973df62d74ec7b41a
ltzu <ltzu@qq.com>
2026-04-28 09:24:36 +0800

    feat: export OMNI analysis classes in autoslice __init__

--- src/autoslice/__init__.py
+++ src/autoslice/__init__.py
@@ -5,5 +5,13 @@ import os
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from .auto_slice_video.autosv import slice_video_by_danmaku
+from .analysis_result import AnalysisResult, Highlight, TrimSuggestion
+from .slice_quality_filter import should_retain_slice
 
-__all__ = ["slice_video_by_danmaku"]
+__all__ = [
+    "slice_video_by_danmaku",
+    "AnalysisResult",
+    "Highlight",
+    "TrimSuggestion",
+    "should_retain_slice",
+]
